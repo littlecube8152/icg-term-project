2790b47705aa57ccf08fede55fc92b1030d9aac6
PixelCat31415 <jimmychiu31415@gmail.com>
2025-05-12 11:56:37 +0800

    feat: helper function for near-zero checks when normalizing vectors

---
+++ include/vecutil.h
@@ -0,0 +1,31 @@
+#ifndef VECUTIL_H_
+#define VECUTIL_H_
+
+#include "glm/glm.hpp"
+#include "glm/gtc/epsilon.hpp"
+
+#include "constants.h"
+
+
+// normalize vec, but fallback to default_vec when vec is near-zero
+// assumes default_vec is already normalized
+template<typename genType>
+inline genType default_normalize(const genType &vec, const genType &default_vec) {
+    auto length = glm::length(vec);
+    if (glm::epsilonEqual(length, (typename genType::value_type)(0), kEpsilon)) {
+        return default_vec;
+    } else {
+        return vec / length;
+    }
+}
+
+template<>
+inline float default_normalize<float>(const float &value, const float &default_value) {
+    if (glm::epsilonEqual(value, 0.0f, kEpsilon)) {
+        return default_value;
+    } else {
+        return value < 0.0f ? -1.0f : 1.0f;
+    }
+}
+
+#endif
--- src/lambertian.cpp
+++ src/lambertian.cpp
@@ -3,6 +3,7 @@
 #include "glm/gtc/epsilon.hpp"
 
 #include "randutil.h"
+#include "vecutil.h"
 #include "constants.h"
 
 
@@ -12,12 +13,7 @@ Lambertian::Lambertian(const glm::vec4 &_albedo): albedo(_albedo) {}
 bool Lambertian::scatter(const Ray &r_in, HitRecord &rec) const {
     [](...){}(r_in);
     auto scatter_dir = rec.normal + rand_unit_length<glm::vec3>();
-    auto length = glm::length(scatter_dir);
-    if (glm::epsilonEqual(length, 0.0f, kEpsilon)) {
-        scatter_dir = rec.normal;
-    } else {
-        scatter_dir /= length;
-    }
+    scatter_dir = default_normalize(scatter_dir, rec.normal);
     rec.scattered = Ray(rec.p, scatter_dir);
     rec.attenuation = albedo;
     return true;
