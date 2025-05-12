c5c7d4845cac15bcb2e04ea2b64f6b2b49916cee
PixelCat31415 <jimmychiu31415@gmail.com>
2025-05-12 11:38:35 +0800

    feat: calculate time to render a frame

--- src/scene.cpp
+++ src/scene.cpp
@@ -1,6 +1,8 @@
 #include "scene.h"
 
 #include <memory>
+#include <chrono>
+#include <iostream>
 
 #include "sphere.h"
 #include "lambertian.h"
@@ -20,5 +22,10 @@ Scene::Scene(): camera(), world() {
 
 
 GLuint Scene::renderAsTexture(GLuint texture_width, GLuint texture_height) {
-    return camera.renderAsTexture(texture_width, texture_height, world);
+    const auto start = std::chrono::high_resolution_clock::now();
+    GLuint texture = camera.renderAsTexture(texture_width, texture_height, world);
+    const auto end = std::chrono::high_resolution_clock::now();
+    const std::chrono::duration<double> elapsed = end - start;
+    std::cerr << "Frame time: " << elapsed << std::endl;
+    return texture;
 }
