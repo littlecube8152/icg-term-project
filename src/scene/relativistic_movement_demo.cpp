b082f06c944d352140c70997afff5f93f5470e1f
littlecube8152 <froakie20161113@gmail.com>
2025-05-30 16:22:40 +0800

    feat: update relativistic movement demo yet again for better demonstration

--- src/scene/relativistic_movement_demo.cpp
+++ src/scene/relativistic_movement_demo.cpp
@@ -11,8 +11,8 @@ SceneRelativisticMovementDemo::SceneRelativisticMovementDemo(const ArgumentParse
         .image_width = static_cast<unsigned>(options.getWidth()),
         .image_height = static_cast<unsigned>(options.getHeight()),
         .vfov = 90,
-        .lookfrom = glm::vec3(-1, 0, -3),
-        .lookat = glm::vec3(0, 0, 0),
+        .lookfrom = glm::vec3(0, 0, -3),
+        .lookat = glm::vec3(1, 0, 0),
         .lookup = glm::vec3(0, 1, 0),
         .sqrt_samples_per_pixel = options.getSampleOption(),
         .max_recursion_depth = options.getDepthOption(),
@@ -27,23 +27,25 @@ SceneRelativisticMovementDemo::SceneRelativisticMovementDemo(const ArgumentParse
     auto material_right = std::make_shared<Metal>(glm::vec4(0.8f, 0.6f, 0.2f, 1.0f), 1.0f);
     InertialFrame slower_frame(glm::vec3(0.2, 0.0, 0.0));
     InertialFrame faster_frame(glm::vec3(0.7, 0.0, 0.0));
-    world.add(std::make_shared<Cube>(glm::vec3(-1.0f, 0.0f, 1.0f), 0.8f, material_right));
+    InertialFrame even_faster_frame(glm::vec3(0.9, 0.0, 0.0));
     world.add(std::make_shared<Cube>(glm::vec3(0.0f, -1000.5f, 1.0f), 2000.0f, material_ground));
-    world.add(std::make_shared<Cube>(glm::vec3(0.0f, 0.0f, 1.0f), 0.8f, material_center));
-    world.add(std::make_shared<Cube>(glm::vec3(1.0f, 0.0f, 1.0f), 0.8f, material_left));
-    world.add(std::make_shared<Cube>(glm::vec3(1.0f, 0.0f, 1.0f), 0.6f, material_bubble));
 
-    world.add(std::make_shared<Cube>(glm::vec3(2.0f, 0.0f, 1.0f), 0.8f, material_left, slower_frame));
-    world.add(std::make_shared<Cube>(glm::vec3(2.0f, 0.0f, 1.0f), 0.6f, material_bubble, slower_frame));
-    world.add(std::make_shared<Cube>(glm::vec3(3.0f, 0.0f, 1.0f), 0.8f, material_right, slower_frame));
+    world.add(std::make_shared<Cube>(glm::vec3(-6.0f, 0.0f, 1.0f), 0.8f, material_right));
+    world.add(std::make_shared<Cube>(glm::vec3(-5.0f, 0.0f, 1.0f), 0.8f, material_center));
+    world.add(std::make_shared<Cube>(glm::vec3(-4.0f, 0.0f, 1.0f), 0.8f, material_left));
+    world.add(std::make_shared<Cube>(glm::vec3(-4.0f, 0.0f, 1.0f), 0.6f, material_bubble));
 
-    world.add(std::make_shared<Cube>(glm::vec3(2.5f, 0.0f, 1.0f), 0.8f, material_left, camera_frame));
-    world.add(std::make_shared<Cube>(glm::vec3(2.5f, 0.0f, 1.0f), 0.6f, material_bubble, camera_frame));
-    world.add(std::make_shared<Cube>(glm::vec3(3.5f, 0.0f, 1.0f), 0.8f, material_right, camera_frame));
+    world.add(std::make_shared<Cube>(glm::vec3(-1.0f, 0.0f, 1.0f), 0.8f, material_left, slower_frame));
+    world.add(std::make_shared<Cube>(glm::vec3(-1.0f, 0.0f, 1.0f), 0.6f, material_bubble, slower_frame));
+    world.add(std::make_shared<Cube>(glm::vec3(0.0f, 0.0f, 1.0f), 0.8f, material_right, slower_frame));
 
-    world.add(std::make_shared<Cube>(glm::vec3(3.0f, 0.0f, 1.0f), 0.8f, material_left, faster_frame));
-    world.add(std::make_shared<Cube>(glm::vec3(3.0f, 0.0f, 1.0f), 0.6f, material_bubble, faster_frame));
-    world.add(std::make_shared<Cube>(glm::vec3(4.0f, 0.0f, 1.0f), 0.8f, material_right, faster_frame));
+    world.add(std::make_shared<Cube>(glm::vec3(3.0f, 0.0f, 1.0f), 0.8f, material_left, camera_frame));
+    world.add(std::make_shared<Cube>(glm::vec3(3.0f, 0.0f, 1.0f), 0.6f, material_bubble, camera_frame));
+    world.add(std::make_shared<Cube>(glm::vec3(4.0f, 0.0f, 1.0f), 0.8f, material_right, camera_frame));
 
-    world.add(std::make_shared<Cube>(glm::vec3(4.0f, 0.0f, 1.0f), 0.8f, material_left, InertialFrame(glm::vec3(0.8, 0.0, 0.0))));
+    world.add(std::make_shared<Cube>(glm::vec3(7.0f, 0.0f, 1.0f), 0.8f, material_left, faster_frame));
+    world.add(std::make_shared<Cube>(glm::vec3(7.0f, 0.0f, 1.0f), 0.6f, material_bubble, faster_frame));
+    world.add(std::make_shared<Cube>(glm::vec3(8.0f, 0.0f, 1.0f), 0.8f, material_right, faster_frame));
+
+    world.add(std::make_shared<Cube>(glm::vec3(11.0f, 0.0f, 1.0f), 0.8f, material_left, even_faster_frame));
 }
