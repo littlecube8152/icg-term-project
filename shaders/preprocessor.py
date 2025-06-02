7994f6bf21bf822e87f52d9b7434fb855da54240
PixelCat31415 <jimmychiu31415@gmail.com>
2025-06-02 20:09:38 +0800

    fix: preprocessor messages and the #include pattern

--- shaders/preprocessor.py
+++ shaders/preprocessor.py
@@ -14,11 +14,11 @@ def parse_shader(filename: str):
     loading_shader.add(filename)
     file = open(os.path.join("shaders", filename), 'r')
     for line_number, line in enumerate(file.read().split('\n')):
-        match = re.search("#include\\s*\"(.*?)\"", line)
+        match = re.search(r'^#include\s*"(.*?)"\s*$', line)
         if match is not None:
             include_source = match[1]
             if include_source in loading_shader:
-                print(f"{filename}:{line_number + 1}: error: including shader file {input_name} cases cyclic inclusion", file=sys.stderr)
+                print(f"{filename}:{line_number + 1}: error: including shader file {include_source} causes cyclic inclusion", file=sys.stderr)
                 exit(1)
             elif include_source in loaded_shader:
                 parsed_shader += f'/// include file "{include_source}" skipped (duplicated include)\n'
@@ -27,7 +27,7 @@ def parse_shader(filename: str):
                 try:
                     parse_shader(include_source)
                 except FileNotFoundError:
-                    print(f"{filename}:{line_number + 1}: error: including shader file {input_name} cases cyclic inclusion", file=sys.stderr)
+                    print(f"{filename}:{line_number + 1}: error: included shader file {include_source} not found", file=sys.stderr)
                     exit(1)
                 parsed_shader += f'/// file "{include_source}" end\n'
         else:
